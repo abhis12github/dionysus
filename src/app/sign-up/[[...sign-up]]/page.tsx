d2c6fc4a411c28b3deac2f621c5b313a5ad0bcff
Abhishek Anand <mailtomeabhi15@gmail.com>
2025-01-27 20:33:02 +0530

    updated signin and signup page ui

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='flex justify-center items-center min-h-screen m-4'>
+      <SignUp />
+    </div>
+  
+  );
 }
